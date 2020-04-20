c73fc0b7e54f373533f5fb399407c777c8f3a402
Kevin Keuning <kkeuning@gmail.com>
2020-04-19 23:01:50 -0700

    converted to gorilla/mux, added path variable support and logging

--- README.md
+++ README.md
@@ -3,14 +3,18 @@ Learning example demonstrating an api server and client.
 
 ## Project status: 
 
-A basic server example is provided using net/http and DefaultServeMux.
+A basic server example is provided using net/http and gorilla/mux
 
 To avoid the need for a database, this example uses as its data a package variable of type []User initialized with three User records.
 
-The net/http ServeMux does not easily support path variables, so the example uses a query parameter to specifiy the id of a user.  A future enhancment will switch to gorilla/mux for support of path variables.
+The net/http ServeMux does not easily support path variables, so the example initially used a query parameter to specifiy the id of a user.  With the introduction of gorilla/mux, path variables are now supported.
 
 A client is not yet provided, but the server can be tested from curl or a web browser:
 1. http://localhost:8090 returns an html Hello, World! message, demonstrating the simplest way that static content can be served.
 2. http://localhost:8090/api/v1/users returns a list of users in json format.
 3. http://localhost:8090/api/v1/users?id=2 returns a single user with id=2 in json format.
+4. http://localhost:8090/api/v1/users/2 returns a single user with id=2 in json format using the path variable.
 
+Logging from the gorilla/handlers package has also been added to the server.
+
+Since the project now has dependencies beyond the standard library, the root of the project has been initialized as a go module.  Both the client and server will exist in the same module.
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var users = []user{
@@ -33,6 +38,34 @@ type user struct {
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello, World!</h1>\n")
+}
+
+func getUser(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	if id != "" {
+		userID, err := strconv.Atoi(id)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Expected id as an integer.")
+			return
+		}
+		for _, u := range users {
+			if u.ID == userID {
+				juser, err := json.Marshal(u)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					fmt.Fprintf(w, "Something went wrong.")
+					return
+				}
+				w.WriteHeader(http.StatusOK)
+				w.Write(juser)
+				return
+			}
+		}
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "User not found.")
+		return
+	}
 }
 
 func listUsers(w http.ResponseWriter, req *http.Request) {
@@ -74,9 +107,11 @@ func listUsers(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	// Initial example is using DefaultServeMux
-	// Will be updated to use gorilla/mux later to add path variables
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/api/v1/users", listUsers)
-	http.ListenAndServe(":8090", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+	r.HandleFunc("/api/v1/users", listUsers)
+	r.HandleFunc("/api/v1/users/{id}", getUser)
+
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	log.Fatal(http.ListenAndServe(":8090", loggedRouter))
 }
